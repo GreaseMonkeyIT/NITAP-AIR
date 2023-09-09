bceca2821c355e3542d880c22b3bc1d32fc4a93f
GreaseMonkeyIT <106567117+GreaseMonkeyIT@users.noreply.github.com>
2023-09-09 21:36:44 +0530

    This is the second commit.
    New features include a new animation for the camera when the game starts

--- World1.gd
+++ World1.gd
@@ -1,14 +1,15 @@
 extends Spatial
 
 onready var plane = $RigidBody2
+onready var anim = $AnimationPlayer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ready():	
+	anim.play("Start")
 
 
 func _process(delta):
@@ -20,4 +21,7 @@ func _process(delta):
 		plane.rotate_z(deg2rad(0.3))
 	if Input.is_action_pressed("Back"):
 		plane.rotate_z(deg2rad(-0.3))
+	
+	
+	anim.playback_speed = 2
 		
